34457183719a13af8dc664a8c4abc7cffc7e5bcd
claudiuchichirau <chichirau.claudiu@yahoo.ro>
2023-06-15 10:58:37 +0300

    inceput impl createAccount

--- src/static/controllers/createAccountController.js
+++ src/static/controllers/createAccountController.js
@@ -4,53 +4,71 @@ const path = require('path');
 const bcrypt = require('bcrypt');
 // const { connectToDatabase, getClient } = require('./database/dbManager');
 
-function handleCreateAccountRequest(req, res) {
-  const filePath = path.join(__dirname, '../pages/createAccount.html');
+function handleCreateAccountSubmit(event) {
+  event.preventDefault(); // Previne comportamentul implicit de reîncărcare a paginii
 
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      res.statusCode = 404;
-      res.end('404 Not Found');
-    } else {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/html');
-      res.end(data);
-    }
-  });
+  const form = document.getElementById('createAccountForm');
+  const formData = new FormData(form);
+
+  const username = formData.get('username');
+  console.log(username); // Afișează valoarea introdusă în câmpul "username"
+
+  const forgotEmail = formData.get('forgotEmail');
+  console.log(forgotEmail); // Afișează valoarea introdusă în câmpul "username"
+
+  const password = formData.get('password');
+  console.log(password); // Afișează valoarea introdusă în câmpul "username"
+
+  const confirmPassword = formData.get('confirmPassword');
+  console.log(confirmPassword); // Afișează valoarea introdusă în câmpul "username"
 }
 
-async function handleCreateAccountSubmit(req, res) {
-  try {
-    const { email, password } = req.body;
+// fetch('/create-account', {
+//   method: 'POST',
+//   body: formData
+// })
+//   .then(response => response.json())
+//   .then(data => {
+//     console.log(data); // Înlocuiește cu logica ta de gestionare a răspunsului
+//   })
+//   .catch(error => {
+//     console.error('Eroare la efectuarea cererii:', error);
+//   });
 
-    // Generarea saltului
-    const salt = await bcrypt.genSalt(10);
 
-    // Criptarea parolei utilizând saltul generat
-    const hashedPassword = await bcrypt.hash(password + salt, 10);
 
-    // Obținerea clientului MongoDB din DBManager
-    const client = getClient();
+// async function handleCreateAccountSubmit(req, res) {
+//   try {
+//     const { email, password } = req.body;
 
-    // Selectați baza de date
-    const database = client.db('Authentication');
+//     // Generarea saltului
+//     const salt = await bcrypt.genSalt(10);
 
-    // Selectați colecția utilizatori
-    const collection = database.collection('users');
+//     // Criptarea parolei utilizând saltul generat
+//     const hashedPassword = await bcrypt.hash(password + salt, 10);
 
-    // Inserați detaliile utilizatorului în baza de date
-    await collection.insertOne({ email, password: hashedPassword, salt });
+//     // Obținerea clientului MongoDB din DBManager
+//     const client = getClient();
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ message: 'Contul a fost creat cu succes' }));
-  } catch (error) {
-    console.error('Eroare la crearea contului:', error);
-    res.statusCode = 500;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ message: 'A apărut o eroare la crearea contului' }));
-  }
-}
+//     // Selectați baza de date
+//     const database = client.db('Authentication');
+
+//     // Selectați colecția utilizatori
+//     const collection = database.collection('users');
+
+//     // Inserați detaliile utilizatorului în baza de date
+//     await collection.insertOne({ email, password: hashedPassword, salt });
+
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'application/json');
+//     res.end(JSON.stringify({ message: 'Contul a fost creat cu succes' }));
+//   } catch (error) {
+//     console.error('Eroare la crearea contului:', error);
+//     res.statusCode = 500;
+//     res.setHeader('Content-Type', 'application/json');
+//     res.end(JSON.stringify({ message: 'A apărut o eroare la crearea contului' }));
+//   }
+// }
 
 module.exports = { handleCreateAccountRequest, handleCreateAccountSubmit };
  
