786be6bbca90cc805a9f5b92173adb0ad1abc8d2
alinaduca <alina_duca@yahoo.com>
2023-06-22 15:21:04 +0300

    Display username in MyAccount

---
+++ src/static/utils/my-account.js
@@ -0,0 +1,8 @@
+fetch('/get-username')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        const userField = document.getElementById('username');
+        userField.innerText = data;
+    })
+    .catch();
